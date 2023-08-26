d76ee81dbce12a96c6aeef7cd2ede77e57161cb8
Ramzi Haidan <ramzihaidan537@gmail.com>
2023-08-26 06:52:40 +0300

    Authentiction

--- fastparcel/settings.py
+++ fastparcel/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
 
     # My addition
-    'bootstrap5'
+    'bootstrap5',
+    
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,7 @@ STATICFILES_DIRS=[
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_URL='sign-in'
+LOGIN_REDIRECT_URL='/'
